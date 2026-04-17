29c749b646263d80497af9873416681f353be816
Nifra Wahaj <nifrawahaj@gmail.com>
2026-04-17 09:26:17 +0500

    feat: add prefect orchestration and deployment logic

--- orchestration/flows.py
+++ orchestration/flows.py
@@ -1,5 +1,6 @@
 from prefect import flow
 from ingestion.pipeline import ingestion_flow
+from prefect.client.schemas.schedules import CronSchedule
 
 @flow(name="nasdaq-master-pipeline")
 def master_pipeline():
@@ -16,4 +17,12 @@ def master_pipeline():
     # prediction_flow()
 
 if __name__ == "__main__":
-    master_pipeline()
+    # This turns the script into a permanent 'worker' 
+    # that creates a Deployment in the UI
+    master_pipeline.serve(
+        name="nasdaq-manual-deployment",
+        tags=["dev", "nasdaq"],
+        description="Manual trigger for the full Nasdaq pipeline",
+        # Optional: Add a schedule here if you want it to run every morning
+        # schedule=CronSchedule(cron="0 9 * * 1-5") 
+    )
